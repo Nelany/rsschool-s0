441d531e21ecb82442597a4b9038443b7d73a5f0
Nelany <svetasweetka@gmail.com>
2023-09-25 13:41:06 +0200

    feat: add progress, time

--- audio-player/index.js
+++ audio-player/index.js
@@ -5,7 +5,12 @@ const backwardButton = document.querySelector(".backward");
 const forwardButton = document.querySelector(".forward");
 let isPlay = false;
 const songs = [
-  { artist: "Beyonce", songName: "Don't Hurt Yourself", audio: "beyonce.mp3", img: "lemonade.png", },
+  {
+    artist: "Beyonce",
+    songName: "Don't Hurt Yourself",
+    audio: "beyonce.mp3",
+    img: "lemonade.png",
+  },
   {
     artist: "Dua Lipa",
     songName: "Don't Start Now",
@@ -16,15 +21,42 @@ const songs = [
 const songData = document.querySelector(".songs-data");
 let currentIndex = 0;
 const songImg = document.querySelectorAll(".song-img");
+let songTime = document.querySelector(".song-time");
+let time = document.querySelector(".current-time");
+const progress = document.querySelector(".progress");
 
 function playAudio() {
   const audio = document.querySelector("audio");
-  audio.currentTime = 0;
+  // audio.currentTime = 0;
   audio.play();
   isPlay = true;
   playPauseButton.classList.add("play");
   playPauseButton.classList.remove("pause");
+
+  audio.addEventListener("timeupdate", function () {
+    console.log("FFFF");
+    curtime = parseInt(audio.currentTime, 10);
+    progress.value = curtime;
+    progress.max = audio.duration;
+
+    const minutes = Math.floor(curtime / 60);
+    const seconds = curtime % 60;
+
+    const formattedTime = `${String(minutes).padStart(2, "0")}:${String(
+      seconds
+    ).padStart(2, "0")}`;
+    time.textContent = formattedTime;
+
+    const allMinutes = Math.floor(progress.max / 60);
+    const allSeconds = Math.floor(progress.max) % 60;
+
+    const allFormattedTime = `${String(allMinutes).padStart(2, "0")}:${String(
+      allSeconds
+    ).padStart(2, "0")}`;
+    songTime.textContent = allFormattedTime;
+  });
 }
+
 function pauseAudio() {
   const audio = document.querySelector("audio");
   audio.pause();
@@ -48,7 +80,7 @@ function previousAudio() {
     currentIndex = songs.length - 1;
   }
   setAudio(currentIndex);
-  playAudio()
+  playAudio();
 }
 
 function nextAudio() {
@@ -58,7 +90,7 @@ function nextAudio() {
     currentIndex = 0;
   }
   setAudio(currentIndex);
-  playAudio()
+  playAudio();
 }
 
 function setAudio(index) {
@@ -76,15 +108,14 @@ function setAudio(index) {
 
   songData.innerHTML = songTemplate;
 
-  songImg.forEach((img)=>{
-    img.src = `./assets/img/${playNow.img}`
-  })
+  songImg.forEach((img) => {
+    img.src = `./assets/img/${playNow.img}`;
+  });
 }
 
 setAudio(currentIndex);
 
 body.addEventListener("click", function (event) {
-  console.log(event.target);
   if (event.target === playPauseButton) {
     playPauseAudio();
   } else if (event.target === backwardButton) {
@@ -92,4 +123,12 @@ body.addEventListener("click", function (event) {
   } else if (event.target === forwardButton) {
     nextAudio();
   }
+});
+
+
+
+progress.addEventListener("change", function (event) {
+  const audio = document.querySelector("audio");
+  audio.currentTime = event.target.value;
+  progress.max = audio.duration;
 });
