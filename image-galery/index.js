464716b7bd8b9162e93fc3b8b87c4bc1962b3508
Nelany <svetasweetka@gmail.com>
2023-10-01 22:01:29 +0200

    feat: add click, clear

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,12 +1,27 @@
 const searchIcon = document.querySelector(".search-icon");
+const clearIcon = document.querySelector(".clear-icon");
 const input = document.querySelector(".input");
 const main = document.querySelector(".main");
 
 searchIcon.addEventListener("click", handleSearch);
+clearIcon.addEventListener("click", handleClear);
+input.addEventListener("keydown", handleKeyPress);
 
 function handleSearch() {
   fetchPhotos(input.value);
   input.focus();
+}
+
+function handleClear() {
+  input.value = "";
+  input.focus();
+}
+
+function handleKeyPress(event) {
+  if (event.keyCode == 13) {
+    fetchPhotos(input.value);
+    input.focus();
+  }
 }
 
 async function fetchPhotos(searchString) {
@@ -25,12 +40,24 @@ async function fetchPhotos(searchString) {
     );
     const tempData = await response.json();
     data = tempData.results;
+  } else {
+    const response = await fetch(
+      "https://api.unsplash.com/photos?per_page=30",
+      {
+        headers: headers,
+      }
+    );
+    data = await response.json();
   }
 
   showImg(data);
 }
 
 function showImg(data) {
+  if (!data || data?.length == 0) {
+    main.innerHTML = `<div class="no-images">There is no images</div>`;
+    return;
+  }
   const links = data.map((el) => el.urls);
   const inner = links
     .map((element) => {
@@ -39,3 +66,4 @@ function showImg(data) {
     .join("");
   main.innerHTML = inner;
 }
+ fetchPhotos();
