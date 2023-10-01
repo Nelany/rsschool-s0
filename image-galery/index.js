72651b8610a3944387507ce8de6ceab431063c8e
Nelany <svetasweetka@gmail.com>
2023-10-01 14:06:50 +0200

    feat: add search

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,13 +1,41 @@
 const searchIcon = document.querySelector(".search-icon");
 const input = document.querySelector(".input");
+const main = document.querySelector(".main");
 
-searchIcon.addEventListener ("click", handleClick)
+searchIcon.addEventListener("click", handleSearch);
 
-function handleClick(event) {
-let value = input.value;
-
-console.log(value);
+function handleSearch() {
+  fetchPhotos(input.value);
+  input.focus();
 }
 
+async function fetchPhotos(searchString) {
+  let data;
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: "Client-ID wcsAWgugNaAnzQ_kCGjBwmjhoNIs8JpUa7ra13YRMR0",
+  };
 
+  if (searchString) {
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?per_page=30&query=${searchString}`,
+      {
+        headers: headers,
+      }
+    );
+    const tempData = await response.json();
+    data = tempData.results;
+  }
 
+  showImg(data);
+}
+
+function showImg(data) {
+  const links = data.map((el) => el.urls);
+  const inner = links
+    .map((element) => {
+      return `<a href="${element.full}" target="_blank" class="img-container" style="background-image:url(${element.thumb})"></a>`;
+    })
+    .join("");
+  main.innerHTML = inner;
+}
